2fa83179c2f6a608896ab9d52b4d6bcd2b39284d
Vasilii Leontev <leovas1972@gmail.com>
2023-08-09 15:21:49 +0700

    remove fixture from function arguments

--- test_google.py
+++ test_google.py
@@ -1,12 +1,12 @@
 from selene.support.shared import browser
 from selene import be, have
 
-def test_one(config_browser):
+def test_one():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
 
-def test_two(config_browser):
+def test_two():
     browser.open('https://google.com')
     browser.element('[name="q"]').should(be.blank).type('xxxrrruuulllsss').press_enter()
     browser.element("[id='result-stats']").should(have.text('Результатов: примерно 0'))
