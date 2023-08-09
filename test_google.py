c8dd4408aa7ab298bd2b109c3b777b4646d9cd3f
Vasilii Leontev <leovas1972@gmail.com>
2023-08-09 12:30:51 +0700

    creating two tests in test_google.py

---
+++ test_google.py
@@ -0,0 +1,12 @@
+from selene.support.shared import browser
+from selene import be, have
+
+def test_one(config_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('yashaka/selene: User-oriented Web UI browser tests in'))
+
+def test_two(config_browser):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('xxxrrruuulllsss').press_enter()
+    browser.element("[id='result-stats']").should(have.text('Результатов: примерно 0'))
